ca95eedc533466673821c8de90682ce467329ddb
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-11-19 13:55:23 +0100

    try to pass control/current to att sevrer

--- src/common/real/control/current.rs
+++ src/common/real/control/current.rs
@@ -1,71 +1,77 @@
 use crate::common::driver::KoradDriver;
 use panduza_platform_core::protocol::CommandResponseProtocol;
-use panduza_platform_core::Error;
-use panduza_platform_core::{
-    spawn_on_command, BidirMsgAtt, Device, DeviceLogger, Interface, SiCodec, SiSettings,
-};
+use panduza_platform_core::{log_info, Error, SiAttServer};
+use panduza_platform_core::{spawn_on_command, Device, DeviceLogger, Interface};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 ///
-///
+/// control/current
 ///
 pub async fn mount<SD: CommandResponseProtocol + 'static>(
     mut device: Device,
     mut interface: Interface,
     driver: Arc<Mutex<KoradDriver<SD>>>,
 ) -> Result<(), Error> {
-    let settings = SiSettings::new("A", 0, 5, 3);
+    //
+    // Start logging
+    let logger = device.logger.clone();
+    logger.info("Mounting 'control/current' class...");
 
     //
-    //
-    let att_current = interface
+    // Create the attribute
+    let att_server = interface
         .create_attribute("current")
-        .with_settings(settings.into())
-        .message()
-        .with_bidir_access()
-        .finish_with_codec::<SiCodec>()
-        .await;
+        .with_rw()
+        .finish_as_si("A", 0, 5, 3)
+        .await?;
 
+    //
+    // Init with a first value
     let v = driver.lock().await.get_iset().await?;
-    att_current.set(SiCodec::from_f32(v, 3)).await.unwrap();
+    att_server.set_from_f32(v).await?;
 
     //
     // Execute action on each command received
-    let logger_for_cmd_event = device.logger.clone();
-    let att_current_for_cmd_event = att_current.clone();
+    let logger_2 = logger.clone();
+    let att_server_2 = att_server.clone();
     spawn_on_command!(
         device,
-        att_current_for_cmd_event,
-        on_command(
-            logger_for_cmd_event.clone(),
-            att_current_for_cmd_event.clone(),
-            driver.clone()
-        )
+        att_server_2,
+        on_command(logger_2.clone(), att_server_2.clone(), driver.clone())
     );
 
     //
-    // Function ok
+    // End of mount
+    logger.info("Mounting 'control/current' class -> OK");
     Ok(())
 }
 
 ///
+/// control/current => triggered when command is received
 ///
-///
-async fn on_command<SD: CommandResponseProtocol + Send>(
+async fn on_command<SD: CommandResponseProtocol>(
     logger: DeviceLogger,
-    mut value_value_attr: BidirMsgAtt<SiCodec>,
+    mut att_server: SiAttServer,
     driver: Arc<Mutex<KoradDriver<SD>>>,
 ) -> Result<(), Error> {
-    while let Some(command) = value_value_attr.pop_cmd().await {
-        //
-        // Log
-        logger.debug(format!("SI current command received '{:?}'", command));
-
-        let v = command.into_f32()?;
-        driver.lock().await.set_iset(v).await?;
-
-        value_value_attr.set(command).await?;
+    while let Some(command_result) = att_server.pop_cmd_as_f32().await {
+        match command_result {
+            Ok(v) => {
+                log_info!(logger, "'control/current' - command received '{:?}'", v);
+                driver.lock().await.set_iset(v).await?;
+                att_server.set_from_f32(v).await?;
+                let real_value = driver.lock().await.get_iset().await?;
+                att_server.set_from_f32(real_value).await?;
+            }
+            Err(e) => {
+                let alert = format!("'control/current' - warning on received command '{:?}'", e);
+                logger.warn(&alert);
+                att_server.send_alert(alert).await;
+                let real_value = driver.lock().await.get_iset().await?;
+                att_server.set_from_f32(real_value).await?;
+            }
+        }
     }
     Ok(())
 }
