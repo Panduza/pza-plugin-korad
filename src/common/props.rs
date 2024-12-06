530ddc7582f0768b4f9d335c5fd1288ad3016d44
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-12-06 21:42:57 +0100

    up

--- src/common/props.rs
+++ src/common/props.rs
@@ -1,20 +1,78 @@
+use panduza_platform_core::InstanceSettings;
+
 ///
 ///
 ///
-struct PowerSupplyProps {
-    min_voltage: f32,
-    max_voltage: f32,
-    min_current: f32,
-    max_current: f32,
+#[derive(Debug)]
+struct PowerSupplySettings {
+    min_voltage: f64,
+    max_voltage: f64,
+    min_current: f64,
+    max_current: f64,
 }
 
-impl PowerSupplyProps {
-    pub fn new() -> Self {
+impl PowerSupplySettings {
+    ///
+    ///
+    ///
+    pub fn new(min_voltage: f64, max_voltage: f64, min_current: f64, max_current: f64) -> Self {
         Self {
-            min_voltage: 0.0,
-            max_voltage: 0.0,
-            min_current: 0.0,
-            max_current: 0.0,
+            min_voltage,
+            max_voltage,
+            min_current,
+            max_current,
+        }
+    }
+
+    ///
+    ///
+    ///
+    pub fn override_from_instance_settings(&mut self, settings: Option<InstanceSettings>) {
+        if let Some(data) = settings {
+            if data.is_object() {
+                if let Some(map) = data.as_object() {
+                    //
+                    //
+                    let min_voltage = map
+                        .get("min_voltage")
+                        .and_then(|v| v.as_f64().and_then(|v| Some(v)));
+                    if let Some(v) = min_voltage {
+                        if v > self.min_voltage {
+                            self.min_voltage = v;
+                        }
+                    }
+                    //
+                    //
+                    let max_voltage = map
+                        .get("max_voltage")
+                        .and_then(|v| v.as_f64().and_then(|v| Some(v)));
+                    if let Some(v) = max_voltage {
+                        if v < self.max_voltage {
+                            self.max_voltage = v;
+                        }
+                    }
+                    //
+                    //
+                    let min_current = map
+                        .get("min_current")
+                        .and_then(|v| v.as_f64().and_then(|v| Some(v)));
+                    if let Some(v) = min_current {
+                        if v > self.min_current {
+                            self.min_current = v;
+                        }
+                    }
+                    //
+                    //
+                    let max_current = map
+                        .get("max_current")
+                        .and_then(|v| v.as_f64().and_then(|v| Some(v)));
+                    if let Some(v) = max_current {
+                        if v < self.max_current {
+                            self.max_current = v;
+                        }
+                    }
+                }
+            }
         }
     }
 }
